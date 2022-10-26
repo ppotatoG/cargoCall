2d53014f5662cbfa1f4e9eeb3635772d09624808
ppotaotG <thgus2270@gmail.com>
2022-10-26 15:43:58 +0900

    최종 수정 change layout - section-compare

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,39 +1,5 @@
 const isMobile = window.innerWidth <= 767;
 
-const compareAnimation = () => {
-    const section = document.querySelector('.section-compare');
-    const list = section.querySelectorAll('.text_contents');
-    const rect = section.getBoundingClientRect();
-
-    section.style.height = `${list.length}00vh`;
-
-    const percent = Math.ceil(Math.abs(rect.top) / rect.height * 100);
-    const curIndex = Math.floor(percent / (100 / list.length))
-    const curPercent = percent * list.length % 100;
-
-    if (list[curIndex]) {
-        list.forEach(item => item.classList.remove('observer'));
-        list[curIndex].classList.add('observer');
-        section.classList.add('observer');
-
-        if(curPercent <= 20) {
-            list[curIndex].style.opacity = `${curPercent / 20}`;
-            [...list[curIndex].children].forEach(item => {
-                item.style.transform = `translateY(${curPercent - 20}px)`;
-            });
-        } else if(curPercent >= 80) {
-            list[curIndex].style.opacity = `${20 / curPercent}`;
-            [...list[curIndex].children].forEach(item => {
-                item.style.transform = `translateY(${curPercent - 80}px)`;
-            })
-        } else {
-            list[curIndex].style.opacity = '1';
-        }
-    } else {
-        return false;
-    }
-}
-
 let isGifRunning = false;
 const mapImg = document.querySelector('.map_img');
 const changeGif = () => {
@@ -61,17 +27,6 @@ const io = new IntersectionObserver((entries, ob) => {
                     target.classList.add('observerIn');
                     setTimeout(() => changeGif(), 2500);
                     ob.unobserve(entry.target);
-                    break;
-                case 'compareAnimation':
-                    if (isMobile) {
-                        if (entry.isIntersecting) {
-                            window.addEventListener('scroll', compareAnimation);
-                        } else {
-                            window.removeEventListener('scroll', compareAnimation);
-                        }
-                    } else {
-                        return false;
-                    }
                     break;
             }
         }
